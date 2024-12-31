5f01aa86233c88888bdce4dd2d89bd8d36c1f390
Gh05t9624 <158307320+Gh05t9624@users.noreply.github.com>
2024-12-31 18:17:20 +0000

    nop

---
+++ SunApp/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser as BaseAbstractUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, username, password, **extra_fields)
+
+class CustomUser(BaseAbstractUser):
+    ROLES = [
+        ('admin', 'Admin'),
+        ('acheteur', 'Acheteur'),
+        ('particulier', 'Particulier'),
+        ('boutiques', 'Boutiques'),
+        ('supermarket', 'Super-Marché'),
+        ('imobilier', 'Imobilier'),
+    ]
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    banner_image = models.ImageField(upload_to='banner_images/', null=True, blank=True)
+    GENRE = [
+        ('homme', 'Homme'),
+        ('femme', 'Femme'),
+    ]
+    SITUATION_MATRIMONIALE = [
+        ('celibataire', 'Célibataire'),
+        ('marie', 'Marié(e)'),
+    ]
+    nom_boutique = models.CharField(max_length=255, null=True, blank=True)
+    marque_dispositif = models.CharField(max_length=255, null=True, blank=True)
+    pays = models.CharField(max_length=50, null=True, blank=True)
+    ville = models.CharField(max_length=50, null=True, blank=True)
+    quartier = models.CharField(max_length=50, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    indicatif_pays = models.CharField(max_length=15, null=True, blank=True)
+    number_phone = models.CharField(max_length=15, null=True, blank=True)
+    rôle = models.CharField(max_length=255, choices=ROLES, default='acheteur')
+    genre = models.CharField(max_length=255, choices=GENRE, default='homme')
+    situation_matrimoniale = models.CharField(max_length=20, choices=SITUATION_MATRIMONIALE, default='celibataire')
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.username if self.username else 'Utilisateur sans nom'
+
+    class Meta:
+        # Vous pouvez ajouter ici des contraintes spécifiques si nécessaire
+        unique_together = ('email',)  # Conserver l'unicité de l'email si désiré
+
+
+    
+class Product(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    CATEGORY_CHOICES = [
+        ('Electronics', 'Électronique'),
+        ('Fashion', 'Mode'),
+        ('Home_Garden', 'Maison & Jardin'),
+        # Ajoutez d'autres catégories selon vos besoins
+    ]
+
+    title = models.CharField(max_length=255)
+    contenu_post = models.TextField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    prix = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    date_creation_post = models.DateTimeField(auto_now_add=True)
+    # ... autres champs ...
+
+    def __str__(self):
+        return self.title
+
+class Facture(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    is_public = models.BooleanField(default=False) 
+    date_creation = models.DateTimeField(auto_now_add=True)
+    
+class Notification(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    message = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)  # Assurez-vous que ce champ existe
+
+    def __str__(self):
+        return self.message
---
+++ SunApp/urls.py
@@ -0,0 +1,51 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .api_views import CustomUserViewSet, ProductViewSet, FactureViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
+
+router = DefaultRouter()
+router.register(r'users', CustomUserViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'factures', FactureViewSet, basename='facture')
+
+urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+
+    # ========== Authentification ==================
+    path('', views.log_in, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.log_out, name='logout'),
+    path('profil', views.profile, name='profil'),
+    path('user-map', views.user_map_view, name='user_map'),
+    
+    # ========== Pages Users ======================
+    path('home', views.home, name='home'),
+    path('boutique', views.boutique, name='boutique'),
+    path('super_marcher', views.super_marcher, name='super_marcher'),
+    path('superette', views.superette, name='superette'),
+    path('particulier', views.particulier, name='particulier'),
+    path('imobilier', views.imobilier, name='imobilier'),
+    path('facture_users', views.facture_users, name='facture_users'),
+    
+    # ========== Pages de Notifications ======================
+    path('notifications/', views.notifications, name='notifications'),
+    path('notifications_view/', views.notifications_view, name='notifications_view'),
+    path('mark-notifications-as-read/', views.mark_notifications_as_read, name='mark_notifications_as_read'),
+    path('check_notifications/', views.check_notifications, name='check_notifications'),
+    
+    # ========== Pages Admins ======================
+    path('SuperAdmin/', views.SuperAdmin, name='SuperAdmin'),
+    
+    # ========== Les Formulaires ================
+    path('product/', views.create_product, name='product'),
+    path('facture/', views.facture, name='facture'),
+    
+    # ========== Details ================
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('profiles/<int:user_id>', views.user_detail, name='user_detail'),
+    path('facture_detail/<int:id>/', views.facture_detail, name='facture_detail'),
+    
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+]
